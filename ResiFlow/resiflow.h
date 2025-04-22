c29263a40e944ef0678d1d846cbfcf39a6628d29
Gabriel Garcia <gabrielgarcia0890@gmail.com>
2025-04-22 14:41:48 -0300

    ResiFlow qt creator

---
+++ ResiFlow/resiflow.h
@@ -0,0 +1,21 @@
+#ifndef RESIFLOW_H
+#define RESIFLOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ResiFlow; }
+QT_END_NAMESPACE
+
+class ResiFlow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ResiFlow(QWidget *parent = nullptr);
+    ~ResiFlow();
+
+private:
+    Ui::ResiFlow *ui;
+};
+#endif // RESIFLOW_H
