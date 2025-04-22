14897bbb9855d3c0d2a00d3e9fe32a886497f0e1
Lokarin <onukiamaral@protonmail.com>
2025-04-22 19:20:58 -0300

    Remoção de ResiFlow e ResiFlow2

--- ResiFlow/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_ResiFlow_FILE /home/lokarin/Documentos/Github/ADSR_Control/ResiFlow/build/Desktop-Debug/ResiFlow)
-set(__QT_DEPLOY_TARGET_ResiFlow_TYPE EXECUTABLE)
--- ResiFlow/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ResiFlow_FILE C:/Users/gabri/OneDrive/Área de Trabalho/ADSR_Control/ResiFlow/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ResiFlow.exe)
-set(__QT_DEPLOY_TARGET_ResiFlow_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ResiFlow_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
