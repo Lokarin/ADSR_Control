1eb4a504b24aecf9bbecbc5357d4cd563eef3e30
Lokarin <onukiamaral@protonmail.com>
2025-04-23 17:13:23 -0300

    4 knobs

--- ResiFlow/resiflow.cpp
+++ ResiFlow/resiflow.cpp
@@ -8,17 +8,37 @@ ResiFlow::ResiFlow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    attackKnob = new CustomKnob(this);
-    attackKnob->setMinimum(0);
-    attackKnob->setMaximum(100);
+    // Nome das Labels
+    QStringList knobNames = {"Attack", "Decay", "Sustain", "Release"};
 
-    attackKnob->setParent(ui->knobPlaceholder);
-    attackKnob->setGeometry(0, 0, ui->knobPlaceholder->width(), ui->knobPlaceholder->height());
-    qDebug() << "Knob size:" << attackKnob->size();
+    QHBoxLayout *mainLayout = qobject_cast<QHBoxLayout*>(ui->knobPlaceholder->layout());
 
-    connect(attackKnob, &QDial::valueChanged, this, [=](int value){
-        ui->attackLabel->setText(QString("Valor: %1").arg(value));
-    });
+    for (const QString &name : knobNames) {
+        // Layout vertical para cada conjunto label+knob
+        QVBoxLayout *knobLayout = new QVBoxLayout();
+
+        QLabel *label = new QLabel(name, this);
+        label->setAlignment(Qt::AlignCenter);
+
+        CustomKnob *knob = new CustomKnob(this);
+        knob->setMinimum(0);
+        knob->setMaximum(100);
+        knob->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        qDebug() << "Knob size:" << knob->size();
+
+        connect(knob, &QDial::valueChanged, this, [=](int value){
+            label->setText(QString("%1: %2").arg(name).arg(value));
+        });
+
+        // Adiciona primeiro o knob e depois a label
+        knobLayout->addWidget(knob);
+        knobLayout->addWidget(label);
+
+        QWidget *container = new QWidget(this);
+        container->setLayout(knobLayout);
+
+        mainLayout->addWidget(container);
+    }
 
 }
 
