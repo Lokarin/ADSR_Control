2a47365550b2d09c3f9939d66ff0d15a99d710ab
Lokarin <onukiamaral@protonmail.com>
2025-04-22 21:36:21 -0300

    Remoção de redundancias

--- ResiFlow/customknob.cpp
+++ ResiFlow/customknob.cpp
@@ -13,36 +13,21 @@ CustomKnob::CustomKnob(QWidget *parent)
 void CustomKnob::paintEvent(QPaintEvent *) {
     //() << "paintEvent chamado!";
 
-    //QPainter painter(this);
-    //painter.setRenderHint(QPainter::Antialiasing);
-    //painter.setRenderHint(QPainter::SmoothPixmapTransform);
-
-    //int side = qMin(width(), height());
-    //QPoint center(width() / 2, height() / 2);
-
-    //// Translada e rotaciona de acordo com o valor do knob
-    //painter.translate(center);
-
-    //// ângulo de rotação do ponteiro (270º de sweep total, começa em -135º)
-    //qreal angle = 135.0 + 270.0 * (value() - minimum()) / (maximum() - minimum());
-    //painter.rotate(angle);
-
-    //painter.drawPixmap(-side / 2, -side / 2, side, side, knobImage);
-    //
-
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setRenderHint(QPainter::SmoothPixmapTransform);
 
     int side = qMin(width(), height());
     QPoint center(width() / 2, height() / 2);
+
+    // Translada e rotaciona de acordo com o valor do knob
     painter.translate(center);
 
+    // ângulo de rotação do ponteiro (270º de sweep total, começa em -135º)
     qreal angle = 135.0 + 270.0 * (value() - minimum()) / (maximum() - minimum());
     painter.rotate(angle);
 
     QPixmap scaled = knobImage.scaled(side, side, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     painter.drawPixmap(-scaled.width() / 2, -scaled.height() / 2, scaled);
-
 }
 
