1e49d9b0fb2bee1a453c115ea24a684c9ca7a07c
Lokarin <onukiamaral@protonmail.com>
2025-04-22 22:06:45 -0300

    Angulações

--- ResiFlow/customknob.cpp
+++ ResiFlow/customknob.cpp
@@ -7,12 +7,10 @@ CustomKnob::CustomKnob(QWidget *parent)
     setMaximum(100);
     setWrapping(false);
     setNotchesVisible(false);
-    qDebug() << "Imagem carregada:" << !knobImage.isNull(); // <- Aqui!
+    qDebug() << "Imagem carregada:" << !knobImage.isNull();
 }
 
 void CustomKnob::paintEvent(QPaintEvent *) {
-    //() << "paintEvent chamado!";
-
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setRenderHint(QPainter::SmoothPixmapTransform);
@@ -20,14 +18,21 @@ void CustomKnob::paintEvent(QPaintEvent *) {
     int side = qMin(width(), height());
     QPoint center(width() / 2, height() / 2);
 
-    // Translada e rotaciona de acordo com o valor do knob
+    // Centro do widget
     painter.translate(center);
 
-    // ângulo de rotação do ponteiro (270º de sweep total, começa em -135º)
-    qreal angle = 135.0 + 270.0 * (value() - minimum()) / (maximum() - minimum());
+    // Calcular angulo
+    qreal angle = -135.0 + 270.0 * (value() - minimum()) / (maximum() - minimum());
+
+    // Rotacionar sistema de coordenada
     painter.rotate(angle);
 
+    // Desenhar Imagem Knob Rotacionada
     QPixmap scaled = knobImage.scaled(side, side, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     painter.drawPixmap(-scaled.width() / 2, -scaled.height() / 2, scaled);
+
+    //// Indicação vermelha
+    //painter.setPen(Qt::red);
+    //painter.drawLine(0, 0, 0, -side / 2);  // Line from center to top
 }
 
