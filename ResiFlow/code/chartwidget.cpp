7a28db2f01af32cd01d7f527e2d029cb620e8808
Lokarin <onukiamaral@protonmail.com>
2025-05-27 22:58:13 -0300

    tema grafico

--- ResiFlow/code/chartwidget.cpp
+++ ResiFlow/code/chartwidget.cpp
@@ -9,8 +9,8 @@ ChartWidget::ChartWidget(QWidget *parent)
     // Cria a serie estilo Spline
     QLineSeries * splineSeries = new QLineSeries();
 
-    this->holdCalculation(10000);
-    this->attackCalculation(this->holdTime, 1, this->maxVol, splineSeries);
+    this->holdCalculation(5000);
+    this->attackCalculation(this->holdTime, 10000, this->maxVol, splineSeries);
 
     // Cria o grafico
     QChart * chart = new QChart();
@@ -18,9 +18,16 @@ ChartWidget::ChartWidget(QWidget *parent)
    
     QValueAxis * yAxis = new QValueAxis;
     yAxis->setRange(0, 1.1*this->maxVol);
+    yAxis->setLinePen(QPen(Qt::white));
+    yAxis->setLabelsColor(Qt::white);
+    yAxis->setGridLinePen(QPen(Qt::gray));
 
     QValueAxis * xAxis = new QValueAxis;
     xAxis->setRange(0, this->maxTime);
+    // Configura o eixo X
+    xAxis->setLinePen(QPen(Qt::white));
+    xAxis->setLabelsColor(Qt::white);
+    xAxis->setGridLinePen(QPen(Qt::gray));
 
     chart->addAxis(yAxis, Qt::AlignLeft);
     chart->addAxis(xAxis, Qt::AlignBottom);
@@ -30,6 +37,12 @@ ChartWidget::ChartWidget(QWidget *parent)
 
     chart->legend()->hide();
     //chart->setTitle("Exemplo com QSplineSeries e QLineSeries");
+    chart->setBackgroundBrush(QBrush(Qt::black));        
+    chart->setPlotAreaBackgroundBrush(QBrush(Qt::black)); 
+    chart->setPlotAreaBackgroundVisible(true);
+
+
+    // Configura o eixo Y
 
     // Seta o grafico a ser mostrado pelo chartwidget, no caso chart, criado em cima ali
     setChart(chart);
@@ -38,6 +51,9 @@ ChartWidget::ChartWidget(QWidget *parent)
 
 void ChartWidget::attackCalculation(double holdTime, int resistencia, double maxVol, QLineSeries *data) {
     data->clear();
+    QPen pen(Qt::red);
+    pen.setWidth(6); 
+    data->setPen(pen);
 
     int numPontos = 20;
 
